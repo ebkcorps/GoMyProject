ccae3c0ba97cf8f5d11d58f63b996f19d2c463eb
VIRENDRA <VIRENDRA@DESKTOP-UITG6CG>
2020-04-14 19:46:42 +0530

    Added some changes.

--- GoMyProject/src/main/java/com/globeop/riskfeed/service/OnBordService.java
+++ GoMyProject/src/main/java/com/globeop/riskfeed/service/OnBordService.java
@@ -1,13 +1,20 @@
 package com.globeop.riskfeed.service;
 
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.globeop.riskfeed.dto.OnBoardFunds;
+import com.globeop.riskfeed.entity.ClientOnboardTable;
 import com.globeop.riskfeed.entity.ClientTable;
 import com.globeop.riskfeed.entity.FundTable;
 import com.globeop.riskfeed.entity.OnBordDto;
+import com.globeop.riskfeed.entity.RiskAggregator;
+import com.globeop.riskfeed.enums.AutomationProcess;
+import com.globeop.riskfeed.enums.IsActive;
+import com.globeop.riskfeed.util.GenricUtil;
 
 @Service
 public class OnBordService {
@@ -17,6 +24,12 @@ public class OnBordService {
 	
 	@Autowired
 	private FundService fundService;
+	
+	@Autowired
+	private RiskAggregatorService riskAggregatorService;
+	
+	@Autowired
+	private ClientOnboardService theClientOnboardService;
 	
 	public void addDetails(OnBordDto onBordDto) {
 				
@@ -51,5 +64,50 @@ public class OnBordService {
 			client.addFund(fund);
 		}		
 		clientService.save(client);
+	}
+	
+	public void addOnboardDetails(OnBordDto onBordDto) {
+		//OnBordDto [clientName=BFAM, clientId=2, riskAggregatorId=1, fundName=null, fundIds=null, setUpDate=2020-04-04, endDate=2020-04-04,
+		//automationProcess=RiskMQ, isActive=Active, comments=CCCCCCCCCCCCCCCCCC, frequency=null, 
+		//onBoardFundsList=[OnBoardFunds [fundName=FUND1, frequency=D], OnBoardFunds [fundName=FUND2, frequency=W], OnBoardFunds [fundName=FUND3, frequency=D,W,M], OnBoardFunds [fundName=FUND4, frequency=null]]]
+	
+		System.out.println("@@@@@@@@@@@@@@ Inside addOnboardDetails @@@@@@@@@@@@@@@@@@@ ");
+		
+		ClientTable client = clientService.findById(onBordDto.getClientId());
+		RiskAggregator riskAggregator = riskAggregatorService.findById(onBordDto.getRiskAggregatorId());
+		System.out.println(riskAggregator);
+		
+		List<OnBoardFunds> onBoardFundsList = onBordDto.getOnBoardFundsList();
+		
+		for(OnBoardFunds funds: onBoardFundsList) {
+			System.out.println("11111111 "+funds.getFundName());
+			FundTable fundTable = fundService.findByFundShortName(funds.getFundName());
+			System.out.println("22222222 "+fundTable.getFundShortName());
+			ClientOnboardTable theClientOnboardTable = new ClientOnboardTable();
+			//theClientOnboardTable.setSetUpDate(new Date());//(GenricUtil.convertStringToDate(onBordDto.getSetUpDate()));
+			theClientOnboardTable.setSetUpDate(GenricUtil.convertStringToDate(onBordDto.getSetUpDate()));
+			theClientOnboardTable.setEndDate(GenricUtil.convertStringToDate(onBordDto.getSetUpDate()));
+			theClientOnboardTable.setAutomationProcess(AutomationProcess.valueOf(onBordDto.getAutomationProcess()));
+			theClientOnboardTable.setIsActive(IsActive.valueOf(onBordDto.getIsActive() ));
+			theClientOnboardTable.setComments(onBordDto.getComments());
+			theClientOnboardTable.setFrequency(funds.getFrequency());
+			theClientOnboardTable.setModified_date(new Date());
+			
+			client.addClientOnboard(theClientOnboardTable);
+			
+			riskAggregator.addClientOnboard(theClientOnboardTable);
+			fundTable.addClientOnboard(theClientOnboardTable);
+			
+			fundService.save(fundTable);
+			
+			
+		}
+		clientService.save(client);
+		riskAggregatorService.save(riskAggregator);
+		
+	}
+	
+	public List<ClientOnboardTable> findByClientAndRiskAggregator(ClientTable theClientTable, RiskAggregator theAggregator) {		
+		return theClientOnboardService.findByClientAndRiskAggregator(theClientTable, theAggregator);
 	}
 }
