246f6c86dec6be2e769199fab73432c731815caf
VIRENDRA <VIRENDRA@DESKTOP-UITG6CG>
2020-04-14 19:39:53 +0530

    Added some changes.

--- GoMyProject/src/main/java/com/globeop/riskfeed/entity/OnBordDto.java
+++ GoMyProject/src/main/java/com/globeop/riskfeed/entity/OnBordDto.java
@@ -1,16 +1,25 @@
 package com.globeop.riskfeed.entity;
 
 import java.util.ArrayList;
-
+import java.util.Date;
 import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Lob;
 
 import com.globeop.riskfeed.dto.OnBoardFunds;
+import com.globeop.riskfeed.enums.IsWaivedOff;
 
 
 public class OnBordDto {
 	
+	private String onBoardForm="";
+	
 	//@NotNull(message = "Please select Client")
+	private String clientName;
+	
 	private int clientId;
 	
 	//@NotNull(message = "Please select RiskAggregator")
@@ -38,9 +47,42 @@ public class OnBordDto {
 	//@NotEmpty(message = "Please select frequency")
 	private String frequency;
 
-	
 	private List<OnBoardFunds> onBoardFundsList = new ArrayList<OnBoardFunds>();
-			
+	
+	// for Development form
+	
+	private int developmentHours;
+	
+	private int developmentCost; 
+	
+	private String isWaivedOff;
+	
+	private String startDate;
+	
+	private String developmentComments;
+	
+	private byte[] ApprovalMail;
+	
+	
+	
+	public String getOnBoardForm() {
+		return onBoardForm;
+	}
+
+
+	public void setOnBoardForm(String onBoardForm) {
+		this.onBoardForm = onBoardForm;
+	}
+
+
+	public String getClientName() {
+		return clientName;
+	}
+
+
+	public void setClientName(String clientName) {
+		this.clientName = clientName;
+	}
 	
 	public List<OnBoardFunds> getOnBoardFundsList() {
 		return onBoardFundsList;
@@ -133,12 +175,85 @@ public class OnBordDto {
 		this.clientId = clientId;
 	}
 
+
+	public int getDevelopmentHours() {
+		return developmentHours;
+	}
+
+
+	public void setDevelopmentHours(int developmentHours) {
+		this.developmentHours = developmentHours;
+	}
+
+
+	public int getDevelopmentCost() {
+		return developmentCost;
+	}
+
+
+	public void setDevelopmentCost(int developmentCost) {
+		this.developmentCost = developmentCost;
+	}
+
+
+
+	public String getIsWaivedOff() {
+		return isWaivedOff;
+	}
+
+
+	public void setIsWaivedOff(String isWaivedOff) {
+		this.isWaivedOff = isWaivedOff;
+	}
+
+
+	public String getStartDate() {
+		return startDate;
+	}
+
+
+	public void setStartDate(String startDate) {
+		this.startDate = startDate;
+	}
+
+
+	public String getDevelopmentComments() {
+		return developmentComments;
+	}
+
+
+	public void setDevelopmentComments(String developmentComments) {
+		this.developmentComments = developmentComments;
+	}
+
+
+	public byte[] getApprovalMail() {
+		return ApprovalMail;
+	}
+
+
+	public void setApprovalMail(byte[] approvalMail) {
+		ApprovalMail = approvalMail;
+	}
+
+
 	@Override
 	public String toString() {
-		return "OnBordDto [clientId=" + clientId + ", riskAggregatorId=" + riskAggregatorId + ", fundName=" + fundName
-				+ ", fundIds=" + fundIds + ", setUpDate=" + setUpDate + ", endDate=" + endDate + ", automationProcess="
-				+ automationProcess + ", isActive=" + isActive + ", comments=" + comments + ", frequency=" + frequency
-				+ "]";
+		return "OnBordDto [onBoardForm=" + onBoardForm + ", clientName=" + clientName + ", clientId=" + clientId
+				+ ", riskAggregatorId=" + riskAggregatorId + ", fundName=" + fundName + ", fundIds=" + fundIds
+				+ ", setUpDate=" + setUpDate + ", endDate=" + endDate + ", automationProcess=" + automationProcess
+				+ ", isActive=" + isActive + ", comments=" + comments + ", frequency=" + frequency
+				+ ", onBoardFundsList=" + onBoardFundsList + "]";
 	}
+
+
+
+
+
+	
+
+
+	
+	
 	
 }
