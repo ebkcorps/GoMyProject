cf1693f9ecf49d0374849a5a4ac92c5fc2e3ceda
VIRENDRA <VIRENDRA@DESKTOP-UITG6CG>
2020-04-14 19:24:52 +0530

    Added frequency its getter setter and toString function.

--- GoMyProject/src/main/java/com/globeop/riskfeed/dto/OnBoardFunds.java
+++ GoMyProject/src/main/java/com/globeop/riskfeed/dto/OnBoardFunds.java
@@ -3,7 +3,9 @@ package com.globeop.riskfeed.dto;
 public class OnBoardFunds {
 	
 	private String fundName;
-
+	
+	private String frequency;
+	
 	public String getFundName() {
 		return fundName;
 	}
@@ -11,6 +13,19 @@ public class OnBoardFunds {
 	public void setFundName(String fundName) {
 		this.fundName = fundName;
 	}
-	
+
+	public String getFrequency() {
+		return frequency;
+	}
+
+	public void setFrequency(String frequency) {
+		this.frequency = frequency;
+	}
+
+	@Override
+	public String toString() {
+		return "OnBoardFunds [fundName=" + fundName + ", frequency=" + frequency + "]";
+	}
+		
 	
 }
