42bd60c5a6c7517bf903d6d8e4d19fb195bd11eb
VIRENDRA <VIRENDRA@DESKTOP-UITG6CG>
2020-04-30 20:47:04 +0530

    Added some changes.

--- GoMyProject/src/main/java/com/globeop/riskfeed/entity/RiskAggregator.java
+++ GoMyProject/src/main/java/com/globeop/riskfeed/entity/RiskAggregator.java
@@ -1,5 +1,6 @@
 package com.globeop.riskfeed.entity;
 
+import java.io.Serializable;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
@@ -13,11 +14,15 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
+//@JsonIgnoreProperties({"clientOnboardSet", "modified_date"})
+//@JsonIgnoreProperties({"clientOnboardSet"})
 @Entity
 @Table(name="RiskAggregator")
-public class RiskAggregator {
+public class RiskAggregator implements Serializable{
 	
 	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -33,9 +38,15 @@ public class RiskAggregator {
 	@Column(name="Modified_date")
 	private Date Modified_date;
 		
-	@JsonManagedReference
+	
+	@JsonManagedReference	
 	@OneToMany(targetEntity = ClientOnboardTable.class, cascade = CascadeType.ALL, mappedBy="riskAggregator") 	
     private Set<ClientOnboardTable> clientOnboardSet;
+	
+	@JsonManagedReference
+	@JsonIgnore
+	@OneToMany(targetEntity = BillTable.class, cascade = CascadeType.ALL, mappedBy="riskAggregator") 	
+    private Set<BillTable> billSet;
 
 	public RiskAggregator() {		
 	}
@@ -91,12 +102,29 @@ public class RiskAggregator {
 		theClientOnboardTable.setRiskAggregator(this);
 		this.clientOnboardSet.add(theClientOnboardTable);
     }
+	
+	public void addBill(BillTable theBillTable) {
+		if(theBillTable==null) {
+			billSet = new HashSet<BillTable>();
+		}
+		theBillTable.setRiskAggregator(this);
+		this.billSet.add(theBillTable);
+    }
 
 
 	@Override
 	public String toString() {
 		return "RiskAggregator [id=" + id + ", RiskAggregatorName=" + RiskAggregatorName + ", RiskAggregatorContact="
-				+ RiskAggregatorContact + ", Modified_date=" + Modified_date + ", clientOnboardSet=" + clientOnboardSet
-				+ "]";
-	}	
+				+ RiskAggregatorContact + ", Modified_date=" + Modified_date + "]";
+	}
+
+
+	/*
+	 * @Override public String toString() { return "RiskAggregator [id=" + id +
+	 * ", RiskAggregatorName=" + RiskAggregatorName + ", RiskAggregatorContact=" +
+	 * RiskAggregatorContact + ", Modified_date=" + Modified_date +
+	 * ", clientOnboardSet=" + clientOnboardSet + "]"; }
+	 */	
+	
+	
 }
