8166794fd7f9262fc027ee61cda70790ff137288
VIRENDRA <VIRENDRA@DESKTOP-UITG6CG>
2020-07-04 16:24:59 +0530

    Added Abstract function findByRiskAggregatorNameStartingWith

--- GoMyProject/src/main/java/com/globeop/riskfeed/repository/RiskAggregatorRepository.java
+++ GoMyProject/src/main/java/com/globeop/riskfeed/repository/RiskAggregatorRepository.java
@@ -1,9 +1,15 @@
 package com.globeop.riskfeed.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+
+
 import com.globeop.riskfeed.entity.RiskAggregator;
 
 /*@RepositoryRestResource(path ="riskAggregators")*/
 public interface RiskAggregatorRepository extends JpaRepository<RiskAggregator, Integer> {
+	
+	public List<RiskAggregator> findByRiskAggregatorNameStartingWith(String riskAggregatorName);
 
 }
