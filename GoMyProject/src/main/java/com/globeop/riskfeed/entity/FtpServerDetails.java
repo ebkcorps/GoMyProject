8b782dd0312d0fcea9645c7fd70cdddf65837a96
BKS <BKS@BKSINGH>
2020-07-14 17:22:04 +0530

    changed GenerATION TYPE TO identity FROM role

--- GoMyProject/src/main/java/com/globeop/riskfeed/entity/FtpServerDetails.java
+++ GoMyProject/src/main/java/com/globeop/riskfeed/entity/FtpServerDetails.java
@@ -19,7 +19,7 @@ public class FtpServerDetails {
 	
 	@Id   
 	@Column(name = "FTPDetailID")
-	@GeneratedValue	(strategy=GenerationType.AUTO)  
+	@GeneratedValue	(strategy=GenerationType.IDENTITY)  
 	private int FTPDetailID;
 	
 	
